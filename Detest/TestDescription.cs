a2a552ad7755ee594b6325fbf8693caca239f8cb
Li <ldmorrow@outlook.com>
2024-07-24 21:20:45 +1000

    Internal

--- Detest/TestDescription.cs
+++ Detest/TestDescription.cs
@@ -1,15 +1,15 @@
 namespace Detest;
 
-public record TestScope(string Description, TestScope? Parent)
+internal record TestScope(string Description, TestScope? Parent)
 {
-    public List<TestSetupMethod> TestBeforeAlls { get; } = [];
-    public List<TestSetupMethod> TestBeforeEachs { get; } = [];
-    public List<TestSetupMethod> TestAfterEachs { get; } = [];
-    public List<TestSetupMethod> TestAfterAlls { get; } = [];
-    public List<TestExecutionMethod> TestMethods { get; } = [];
-    public List<TestScope> Children { get; } = [];
+    internal List<TestSetupMethod> TestBeforeAlls { get; } = [];
+    internal List<TestSetupMethod> TestBeforeEachs { get; } = [];
+    internal List<TestSetupMethod> TestAfterEachs { get; } = [];
+    internal List<TestSetupMethod> TestAfterAlls { get; } = [];
+    internal List<TestExecutionMethod> TestMethods { get; } = [];
+    internal List<TestScope> Children { get; } = [];
 }
 
-public record TestSetupMethod(Func<Task> Body);
+internal record TestSetupMethod(Func<Task> Body);
 
-public record TestExecutionMethod(string Description, Func<Task> Body);
+internal record TestExecutionMethod(string Description, Func<Task> Body);
